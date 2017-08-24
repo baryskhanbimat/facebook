22f97cddf8561548e8bb906c7227b0517eccca13
Baryskhan Bimat <baryskhan.bimat@ce.sdu.edu.kz>
2017-08-24 19:47:26 +0600

    Inserting new posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'home/list'
   root 'home#list'
 
+  resources :posts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

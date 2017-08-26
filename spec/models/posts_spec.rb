359291a3b971a309947d0753c431ce9e2f8b0c37
Baryskhan <baryskhan.bimat@ce.sdu.edu.kz>
2017-08-26 22:42:59 +0600

    Add shoulda matchers

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Posts, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  describe Post do
-    it 'should be valid' do
-      @post.should be_valid
-    end
-  end
+RSpec.describe Post, type: :model do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
 end
